f46a302edb7f4ffa0deb43a94c4e7cf79c0f30a1
Vishal Garg <vishugarg10004@gmail.com>
2020-11-09 14:00:30 +0530

    [Vishal] Welcome message

---
+++ Employee Payroll system MSTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Employee_Payroll_system_MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ EmployeePayrollSystemThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollSystemThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
